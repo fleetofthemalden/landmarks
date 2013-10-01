c1cc9bee6e5135feeaa542e7b6cf3969472db74c
Kenneth Cohen <kennethacohen@gmail.com>
2013-10-01 16:48:55 -0400

    m

---
+++ landmarks/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Landmarks::Application.config.secret_key_base = 'cf39e2fae83ebb0506dec0340c83732dbefb1a7cd829eb0d035c18796fdef568ca9bdd1127f59e7347eded039382629e8caa94a211336644e46ffac6c2294a3e'
